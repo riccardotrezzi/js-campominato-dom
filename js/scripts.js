0d4753d15b06ab533d688c6e752cfd8441cfc9e6
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-08-16 18:11:48 +0200

    Sistemato errore

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,45 @@
 const startButton = document.getElementById('start-button');
 
-startButton.addEventListener ('click', function (){
+startButton.addEventListener('click', function (){
+
     const gridContainer = document.getElementById('grid-container');
 
-    for (let i = 1; i <= 100; i++) {
+    const diffLevels = document.getElementById('levels');
+    console.log(diffLevels.value);
+
+    let numberCells = 100;
+    
+    if (diffLevels.value == '2') {
+        numberCells = 81;
+    }
+    else if (diffLevels.value == '3') {
+        numberCells = 49;
+    }
+
+    gridContainer.innerHTML = '';
+
+    for (let i = 1; i <= numberCells; i++) {
 
         const newCell = document.createElement('div');
         newCell.innerHTML = i;
-        gridContainer.append(newCell);
 
+        
+        if (numberCells == 100){
+            newCell.classList.add('easy');
+        }
+        else if (numberCells == 81){
+            newCell.classList.add('medium');
+        }
+        else if (numberCells == 49){
+            newCell.classList.add('hard');
+        }
+            
         newCell.addEventListener ('click', function (){
-            //console.log('this', this, typeof this);
 
             this.classList.add('click-card');
             console.log(i);
         })
+
+        gridContainer.append(newCell);
     }
 })
